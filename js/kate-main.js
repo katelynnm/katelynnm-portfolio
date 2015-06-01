76a45fcea6e991090f63bcecfc7233971cf849c3
Kate Miller <katelynnm.design@gmail.com>
2015-06-01 18:16:04 -0400

    Update

--- js/kate-main.js
+++ js/kate-main.js
@@ -1,31 +1,54 @@
-var today = new Date(),
-	year = today.getFullYear();
-
-			
-var  mn = $(".main-nav");
-    mns = "main-nav-scrolled";
-    hdr = $('header').height();
-
-$(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(mns);
-  } else {
-    mn.removeClass(mns);
-  }
-});
-
 
 // external js: isotope.pkgd.js
 
 $( document ).ready( function() {
+
+
+  // Date in footer
+
+  var today = new Date(),
+    year = today.getFullYear();
+
+
+  // Key Skills Hover
+
+  $(function(){
+    $(".portfolio-thumbnail").click(function(){
+      $(this).toggleClass("active");
+
+    });
+    // $(".portfolio-thumbnail").mouseout(function(){
+    //   $(this).removeClass("active");
+
+    // });
+    
+  });
+
+        
+  // Navigation toggle
+
+  var  mn = $(".main-nav");
+      mns = "main-nav-scrolled";
+      hdr = $('header').height();
+
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > hdr ) {
+      mn.addClass(mns);
+    } else {
+      mn.removeClass(mns);
+    }
+  });
+
+
+
   // init Isotope
   var $container = $('.isotope').isotope({
     itemSelector: '.portfolio-cover',
     layoutMode: 'fitRows',
     getSortData: {
-      name: '.name',
-      symbol: '.symbol',
-      number: '.number parseInt',
+      // name: '.name',
+      // symbol: '.symbol',
+      // number: '.number parseInt',
       category: '[data-category]',
       weight: function( itemElem ) {
         var weight = $( itemElem ).find('.weight').text();
@@ -37,15 +60,15 @@ $( document ).ready( function() {
   // filter functions
   var filterFns = {
     // show if number is greater than 50
-    numberGreaterThan50: function() {
-      var number = $(this).find('.number').text();
-      return parseInt( number, 10 ) > 50;
-    },
+      // numberGreaterThan50: function() {
+      //   var number = $(this).find('.number').text();
+      //   return parseInt( number, 10 ) > 50;
+      // },
     // show if name ends with -ium
-    ium: function() {
-      var name = $(this).find('.name').text();
-      return name.match( /ium$/ );
-    }
+      // ium: function() {
+      //   var name = $(this).find('.name').text();
+      //   return name.match( /ium$/ );
+      // }
   };
 
   // bind filter button click
@@ -70,11 +93,15 @@ $( document ).ready( function() {
       $( this ).addClass('is-checked');
     });
   });
+
+
+  // Date in footer
+  document.getElementById('year').innerHTML = year;
   
 });
 
 
-document.getElementById('year').innerHTML = year;
+
 
 
 // $('#filters').on( 'click', 'button', function() {
